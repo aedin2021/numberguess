06609bf12f15c3e792d5e47aaa63893d39a571e3
aedin <aedin.rundell.2021@student.lhschools.org>
2018-09-21 14:12:34 -0400

    5-100

--- src/numberguess.java
+++ src/numberguess.java
@@ -7,16 +7,28 @@ public class numberguess {
         key = new Scanner(System.in);
         int pickednumber;
         int imput;
+        String playAGame;
+
 
         System.out.println("test your luck and get the randomly generated number. (Note this only goes to 100.)");
         imput = key.nextByte();
         pickednumber = (int)(Math.random()*100);
+        do {
+            if (imput < pickednumber) {
+                System.out.println("too low please guess again.");
+                imput = key.nextByte();
+            } else if (imput > pickednumber) {
+                System.out.println("Too high guess again.");
+                imput = key.nextByte();
+            }
+        }while (imput != pickednumber);
+        if (imput == pickednumber) {
+            System.out.println("congratulations on wining! You got the 1 and "+ pickednumber+" chance of seeing this text!");}
 
-        System.out.print("And the answer is ");
-        System.out.print(pickednumber);
 
-        if (imput == pickednumber){System.out.println("congratulations on wining! You got the 1 and 100 chance of seeing this text!");
-        } else if (imput != pickednumber){System.out.println(", sorry, you lost. Please try again.");}
+
+
+
 
     }
 }
