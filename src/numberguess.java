894f5a74b1084b16bd0f6818dad538147104b6ec
Unknown <aedin.rundell.2021@student.lhschools.org>
2018-10-17 23:52:56 -0400

    11-100

--- src/numberguess.java
+++ src/numberguess.java
@@ -7,69 +7,66 @@ import java.util.Scanner;
 
                 key = new Scanner(System.in);
 
-                int pickednumber;
 
                 int imput;
-
+                int pickednumber;
                 int playAGame;
-
+                int tries = 0;
                 int maxtries;
 
                 maxtries = 10;
-                for (int tries = 0; tries<maxtries;tries++){
-                do {
-
-                    System.out.println("test your luck and get the randomly generated number you only have 10 chances to do it, so be carful. (Note this only goes to 100.)");
-
-                    imput = key.nextByte();
-
-                    pickednumber = (int) (Math.random() * 100);
-
-                    while (imput != pickednumber) {
 
 
-                        if (imput < pickednumber) {
+                    do {
+                        imput = key.nextByte();
+                        System.out.println("test your luck and get the randomly generated number you only have 10 chances" +
+                                " to do it, so be careful. (Note this only goes to 100.)");
 
-                            System.out.println("too low please guess again.");
 
-                            imput = key.nextByte();
+                        while (maxtries > tries) {
+                            tries++;
 
-                        } else if (imput > pickednumber) {
+                            pickednumber = (int) (Math.random() * 100);
 
-                            System.out.println("Too high guess again.");
 
-                            imput = key.nextByte();
+                            while (imput != pickednumber) {
+
+
+                                if (imput < pickednumber) {
+
+                                    System.out.println("too low please guess again.");
+
+                                    imput = key.nextByte();
+
+                                } else if (imput > pickednumber) {
+
+                                    System.out.println("Too high guess again.");
+
+                                    imput = key.nextByte();
+                                }
+                            }
                         }
-                    }
-
-                    if (imput == pickednumber) {
-
-                        System.out.println("congratulations on wining! You got the 1 and " + pickednumber + " chance of seeing this text!");
-
-                        System.out.println("Would you like to play again?(1 to continue/2 to stop)");
 
 
-                    }
+                        if (imput == pickednumber) {
+
+                            System.out.println("congratulations on wining! You got the 1 and " + pickednumber +
+                                    " chance of seeing this text!");
+
+                            System.out.println("Would you like to play again?(1 to continue/2 to stop)");
 
 
-
-                    playAGame = key.nextByte();
-
-                }
-                while (playAGame == 1);
-
-                if (playAGame == 2){System.out.println("We hope you had fun please play again.");}
+                        }
 
 
+                        playAGame = key.nextByte();
 
-    }
-}}
-/*  if (answer.equals("n")) {
 
-                    playAGame = false;
+                    }  while (playAGame == 1) ;
 
+                if (playAGame == 2) {
+                    System.out.println("We hope you had fun please play again.");
                 }
 
-                else {playAGame = true;}*/
-//maxtries=10
-//for(int tries=0;tries>maxtries;tries++;){coad}
+            }}
+
